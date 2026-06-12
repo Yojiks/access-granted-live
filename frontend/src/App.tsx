ba1bd1fa492e5ca2f27e802ce5824d852d3c7e78
Yojiks <MrSplendidYoutube@yandex.ru>
2026-06-12 10:03:27 +0300

    chore: scaffold frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="app-shell">
+      <section className="overlay-frame">
+        <p>BOOTING BREACH SIMULATOR...</p>
+      </section>
+    </main>
+  );
+};
+
+export default App;
